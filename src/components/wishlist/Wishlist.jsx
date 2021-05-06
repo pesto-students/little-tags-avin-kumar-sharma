71e167a29de01b4a13e2a240a52a9c8124fd3803
Krishtyagi277 <krishtyagi277@gmail.com>
2021-05-06 20:56:49 +0530

    Dummy wishlist

--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -4,18 +4,24 @@ import withAuthorization from "../Session/withAuthorization";
 import "./style.css";
 
 function WishList() {
+    const cartEntry = {
+        price: 22.3,
+        product: {category: "men's clothing",
+        description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        id: 2,
+        image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+        price: 22.3,
+        title: "Mens Casual Premium Slim Fit T-Shirts ",},
+        productId: "2",
+        quantity: 1,
+    }
     return (
         <div className="wishlist-page-section">
             <div className="wishlist-section-left">
                 
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
-                <CartEntry />
+                <CartEntry entryData={cartEntry}/>
+                <CartEntry entryData={cartEntry}/>
+               
             </div>
             
         </div>
